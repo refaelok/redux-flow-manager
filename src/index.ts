897fd4959c3becba627c737f6dc4a804f9795ee3
refaelok <refaeloknin@gmail.com>
2020-07-09 23:41:50 +0300

    - fix package json

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import FlowManagerAPI from './flowManager';
 import { SubFlowsConfig } from './subFlowMachine/types';
 
 export { flowManagerReducer } from './store/reducer';
+export * from './subFlowMachine/types';
 export default (store: Store, sliceName: string, flowsConfig: SubFlowsConfig, stepsConfig: any) => {
 	if (!store || !sliceName || !flowsConfig || !stepsConfig) {
 		throw new Error(`store, sliceName, stepsConfig and flowsConfig are mandatory to create FlowManagerAPI. 
