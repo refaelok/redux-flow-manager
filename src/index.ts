0f79985e15fb7df14939be1e9a7b534aad9155a0
refaelok <refaeloknin@gmail.com>
2020-07-10 00:34:13 +0300

    - fix package json

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import StoreAPI from './store';
 import FlowManagerAPI from './flowManager';
 import { SubFlowsConfig } from './subFlowMachine/types';
 
+export { default as FlowManagerAPI } from './flowManager';
 export { flowManagerReducer } from './store/reducer';
 export * from './subFlowMachine/types';
 export default (store: Store, sliceName: string, flowsConfig: SubFlowsConfig, stepsConfig: any) => {
